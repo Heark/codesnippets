a143da8c1035795716e42f1aa7ee5d1fb759b1d5
Heark <hearkofsorcerey@gmail.com>
2015-07-27 17:55:48 -0400

    Update shuffle.js

--- javascript/shuffle.js
+++ javascript/shuffle.js
@@ -1,14 +1,12 @@
 // Simple shuffle function just paste in your code anywhere and start using
 // Made for simplifying getting values from array's randomly..
-
-function shuffle(contents){
+function shuffle(contents) {
     this.contents = contents;
-   var method = this.contents[Math.floor(Math.random() * this.contents.length)]; 
-return method;
-    
+    var method = this.contents[Math.floor(Math.random() * this.contents.length)];
+    return method;
+
 }
 
 // Example
 arr = ["foo", "bar", "tree"];
 console.log(shuffle(arr));
-
